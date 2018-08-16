0633c3f476a5534416e26365ca261b216556a1f9
GT-Frappuccino <39241070+GT-Frappuccino@users.noreply.github.com>
2018-08-16 16:40:22 +0900

    Add files via upload

---
+++ stepper_motor.ino
@@ -0,0 +1,51 @@
+int num;
+int pulse=0; 
+int value=0; 
+// pulse 1000 30cm
+  void setup()  
+  { 
+   pinMode(6, OUTPUT); //Enable 
+   pinMode(5, OUTPUT); //Step 
+   pinMode(4, OUTPUT); //Direction 
+  
+   digitalWrite(6,LOW); 
+   Serial.begin(9600);
+   Serial.setTimeout(1);
+  } 
+
+void loop()  
+{ 
+  if (Serial.available())
+  {
+   num = Serial.parseInt();
+
+   if(pulse > 0)
+   {
+    digitalWrite(4,HIGH); 
+    
+    for(pulse ; pulse > 0; pulse--) 
+    { 
+      digitalWrite(5,HIGH); 
+      delayMicroseconds(500); 
+      digitalWrite(5,LOW); 
+      delayMicroseconds(200); 
+     }
+    
+   }
+
+
+    if (pulse <0)
+    {
+     digitalWrite(4,LOW); 
+     for(pulse ; pulse < 0; pulse++) 
+     { 
+      digitalWrite(5,HIGH); 
+      delayMicroseconds(500); 
+      digitalWrite(5,LOW); 
+      delayMicroseconds(200); 
+     }
+    }
+  }
+  
+  } 
+
