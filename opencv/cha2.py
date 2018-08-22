1db617dea8cc18a0a5e39c83fcda9a826741ffee
xypiao97 <36954727+xypiao97@users.noreply.github.com>
2018-08-23 00:59:27 +0900

    Update cha2.py

--- opencv/cha2.py
+++ opencv/cha2.py
@@ -1,21 +1,29 @@
 import cv2
 import numpy as np
+import argparse
+import socket
+import time
 
-x = []
-y = []
-G_avg = 0
+client = socket.socket()
+#HOST = "169.254.7.25"
+HOST = "192.168.1.105"
+PORT = 5050
+BUFSIZE = 4
+ADDR = (HOST, PORT)
 
-cx = -1
-cy = -1
+client.connect(ADDR)
+print('Connected to', HOST)
 
+current_position = 2325
 firstFrame = None
 
 cap = cv2.VideoCapture(0)
-print(len(x))
 
 while True:
 	ret, frame = cap.read()
 
+	if ret is False:
+		print("Cam Error")
 	frame = cv2.resize(frame, (800, 600))
 	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 	gray = cv2.GaussianBlur(gray, (31, 31), 0)
@@ -29,47 +37,28 @@ while True:
 	thresh = cv2.threshold(frameDelta, 15, 255, cv2.THRESH_BINARY)[1]
 	thresh = cv2.dilate(thresh, None, iterations=2)
 	contours = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[1]
-
+	
 	if contours:
 		cnts = max(contours, key = lambda x: cv2.contourArea(x))
 		m = cv2.moments(cnts)
-		((xloc, yloc), radius) = cv2.minEnclosingCircle(cnts)
-		cv2.circle(frame, (int(xloc),int(yloc)), int(radius), (0, 255, 0), 10)
 
 		if m['m00'] > 0:
-			cx = (m['m10']/m['m00'])
-			cy = (m['m01']/m['m00'])
-			cv2.circle(frame, (int(cx), int(cy)), 1, (0, 255, 0), 10)
-			x.append(cx)
-			y.append(cy)
+			cx = int(m['m10']/m['m00'])
+			cy = int(m['m01']/m['m00'])
+			print(cy)
+			cv2.circle(frame, (0, cy), 6, (0,0,255), 10)
 
-	if (3 <= len(x)) or (3 <= len(y)):
+			move_steps = int(cy * (4650/600) - current_position)
+			current_position = current_position + (move_steps)
+			if current_position < 0:
+				current_position = 0
+			if current_position > 4650:
+				current_position = 4650
+			move_steps = move_steps + 4650
+			client.send(str(move_steps).encode())
 
-		x_compare = x[0] - x[1]
-		if x_compare < 0:
-			del x[:]
-			del y[:]
-			G_avg = 0
-			cv2.imshow("1", frame);
-			key = cv2.waitKey(1)
-			if key == 27:
-				break
-			preFrame = gray
-			continue
-
-		G_avg = int( (((y[1] - y[0]) / (x[1] - x[0])) + ((y[2] - y[1]) / (x[1] - x[0]))) / 2 )
-		x_final = 0
-		y_final = G_avg * (x_final - x[0]) + y[0]
-
-		cv2.line(frame, (int(x[0]), int(y[0])), (int(x_final), int(y_final)), (0,0,255), 10 )
-
-		if (cx <= 10):
-			del x[:]
-			del y[:]
-			G_avg = 0
-
-	cv2.imshow("1", frame);
-
+	cv2.imshow("2", frame)
+	#cv2.imshow("1", thresh)
 	key = cv2.waitKey(1)
 	if key == 27:
 		break
