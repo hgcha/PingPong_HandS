1729df6e6588d7498f33c7c8c84a17e71592acbe
xypiao97 <36954727+xypiao97@users.noreply.github.com>
2018-08-25 15:46:34 +0900

    Update cha2.py

--- opencv/cha2.py
+++ opencv/cha2.py
@@ -5,21 +5,21 @@ import time
 import serial
 
 client = socket.socket()
-#HOST = "169.254.7.25"
-HOST = "192.168.1.105"
-PORT = 5052
+HOST = "169.254.7.25"
+#HOST = "192.168.1.105"
+PORT = 5050
 BUFSIZE = 4
 ADDR = (HOST, PORT)
 
 client.connect(ADDR)
 print('Connected to', HOST)
 
-robot = serial.Serial("/dev/ttyUSB1", 9600)
+robot = serial.Serial("/dev/ttyUSB0", 9600)
 
-current_position = 2325
+current_position = 2300
 firstFrame = None
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(1)
 
 while True:
 	stime = time.time()
@@ -29,6 +29,7 @@ while True:
 		print("Cam Error")
 
 	frame = cv2.resize(frame, (800, 600))
+	cv2.line(frame, (0, 300), (800, 300), (0,0,255), 5)
 	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 	gray = cv2.GaussianBlur(gray, (31, 31), 0)
 
@@ -39,7 +40,7 @@ while True:
 
 	frameDelta = cv2.absdiff(firstFrame, gray)
 	thresh = cv2.threshold(frameDelta, 15, 255, cv2.THRESH_BINARY)[1]
-	thresh = cv2.dilate(thresh, None, iterations=2)
+	thresh = cv2.dilate(thresh, None, iterations=5)
 	contours = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[1]
 	
 	if contours:
@@ -48,24 +49,31 @@ while True:
 		m = cv2.moments(cnts)
 		cx = int(m['m10']/m['m00'])
 		cy = int(m['m01']/m['m00'])
-		print(cy)
-		cv2.circle(frame, (0, cy), 6, (0,0,255), 10)
+		cv2.circle(frame, (0, cy), 6, (255,0,0), 10)
+		cv2.circle(frame, (0, 20 * int(cy / 20)), 6, (0,255,0), 10)
 
-		# send_x = int( cx/10 )
-		if cx in range(10, 50):
-			robot.write(b'1')
-		else :
-			robot.write(b'0')
-		# robot.write()
+		if cx in range(0, 150):
+			a = 1
+		else:
+			a = 0
+		robot.write(str(a))
 
-		move_steps = int(cy * (4650/600) - current_position)
-		current_position = current_position + (move_steps)
-		# if current_position < 0:
-		# 	current_position = 0
-		# elif current_position > 4650:
-		# 	current_position = 4650
-		move_steps = move_steps + 4650
-		client.send(str(move_steps).encode())
+		move_steps = int(cy * 4600 / 600 - current_position)
+
+		if move_steps > 460:
+			quotient = int(move_steps / 230)
+			print("1")
+			move_steps1 = 460 * quotient
+			print(move_steps1 + 4600)
+			client.send(str(move_steps1 + 4600).encode())
+			current_position = move_steps1 + current_position
+		elif move_steps < -460:
+			quotient = int(move_steps / 230) + 1
+			print("-1")
+			move_steps1 = 460 * quotient
+			print(move_steps1 + 4600)
+			client.send(str(move_steps1 + 4600).encode())
+			current_position = move_steps1 + current_position
 
 	key = cv2.waitKey(1)
 	if key == 27:
@@ -75,8 +83,9 @@ while True:
 
 	t_time = round(time.time() - stime, 3)
 	t_text = str(t_time)
-	cv2.putText(frame,t_text, (700, 45), cv2.FONT_HERSHEY_DUPLEX, 1, (0, 255, 0))
+	cv2.putText(frame,t_text, (700, 45), cv2.FONT_HERSHEY_DUPLEX, 1, (0, 0, 255))
 	cv2.imshow("1", frame)
+	cv2.imshow("2", frameDelta)
 
 cap.release()
 cv2.destroyAllWindows()
